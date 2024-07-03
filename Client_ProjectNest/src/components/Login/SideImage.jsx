ced52514a203f9a3746058d378644f81824c099f
Mohit Shahi <moheetshahi92@gmail.com>
2024-07-03 20:54:41 +0545

    ku logo path fixed

--- Client_ProjectNest/src/components/Login/SideImage.jsx
+++ Client_ProjectNest/src/components/Login/SideImage.jsx
@@ -4,7 +4,7 @@ export function SideImage() {
   return (
     <div className="flex justify-center items-center m-10 md:w-30 xl:w-4/12 lg:w-40 ">
       <img
-        src="/public/productivity.png"
+        src="/kulogo.png"
         alt="pic"
         className=" h-36  w-full md:w-30 lg:w-40"
       />
